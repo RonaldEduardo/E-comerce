fe5326bc845e06be86147b3924662d6c62c88d53
ronaldEduardo <ronal29eduardo@gmail.com>
2023-10-20 00:58:41 -0300

    Criação do lista_categoria

---
+++ super23/Adm/lista_categoria.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Lista de Categoria</title>
+  <link rel="stylesheet" href="./lista.css">
+</head>
+
+<body>
+  <?php
+  include("funçãoLista.php");
+  $CamposExibir = array("CodCategoria, Descricao");
+  lista('CATEGORIAS', $CamposExibir, true, true, false);
+  ?>
+  <br>
+  <button onclick="window.location.href='opcoes.html'">Opções do Administrador</button>
+</body>
+
+</html>
