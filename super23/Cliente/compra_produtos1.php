dc4ecb2d182068241634a21fce2a3df0d0628bc8
ronaldEduardo <ronal29eduardo@gmail.com>
2023-11-07 21:44:25 -0300

    Função de compra

--- super23/Cliente/compra_produtos1.php
+++ super23/Cliente/compra_produtos1.php
@@ -1,5 +1,6 @@
 <?php
 include("../conexao.php");
+date_default_timezone_set('America/Sao_Paulo');
 
 
 
@@ -8,8 +9,26 @@ $Carrinho = array(
 );
 
 if (isset($_POST['Compra'])) {
-  print("compra realizada");
+  $Quantidade = $_POST['Quantidade'];
+  $ValorTotal = $_POST['Valor'] * $Quantidade;
+  $CodCliente = $_POST['CodCliente'];
+  $CodProduto = $_POST['CodProduto'];
+  $DataAtual = date("Y-m-d");
+  $HoraAtual = date("H:i:s");
+  $NomeProduto = $_POST["Nome"];
+
+  $Query = "INSERT INTO compras (DataCompra, ValorTotal, CodCliente, CodProduto, Quantidade, HoraCompra) ";
+  $Query .= "VALUES ('$DataAtual', '$ValorTotal', '$CodCliente', '$CodProduto', '$Quantidade', '$HoraAtual')";
+
+  $resultado = mysqli_query($ConexaoId, $Query);
+
+  if ($resultado) {
+    echo "Compra de " . $Quantidade . " " . $NomeProduto . " realizada com sucesso";
+  } else {
+    echo "Erro ao realizar a compra. Detalhes do erro: " . mysqli_error($ConexaoId);
+  }
 }
+
 if (isset($_POST['Adicionar'])) {
   $Produto = $_POST['$CodProduto'];
   $Nome = $_POST['Nome'];
